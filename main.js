4258aef88c7c68466dee39db24f0ea2c32eee4b6
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-19 10:26:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// JavaScript Code
+// SIMPLE ADDITION IPO CALCULATOR BY Oliver Lo
+
+
+// Input
+let number1 = +prompt("Enter a number:");
+let number2 = +prompt("Enter another number;");
+
+// Process
+let total = number1 + number2;
+
+// Output
+alert ("Calculating...")
+alert (`${number1} + ${number2} = ${total}`)
